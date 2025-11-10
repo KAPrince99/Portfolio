e97e58d240e291acb4c6edb5049486753226cf8e
Prince Amanor Kabutey <facultywave124@gmail.com>
2025-11-10 13:21:05 -0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,48 +3,8 @@ import { DockDemo } from "@/components/ui/dockComponent";
 export default function Home() {
   return (
     <main className="flex flex-col">
-      <section className="flex items-center justify-center mt-10 sm:mt-30 mx-5 sm:mx-120 bg-white p-4 rounded-md">
-        <p className="text-[22px] sm:text-3xl text black">
-          Prince, Amanor. creating cool software that makes cool people&apos;s
-          life easier. Junior Fullstack Engineer, unsatisfiably curious about
-          beautiful interfaces and relentlessly devoted to fine, detailed
-          polished functionalities.
-        </p>
-      </section>
-      <section className="flex items-center justify-center mt-10 sm:mt-30 mx-5 sm:mx-120 bg-white p-4 rounded-md">
-        <p className="text-[22px] sm:text-3xl text black">
-          Prince, Amanor. creating cool software that makes cool people&apos;s
-          life easier. Junior Fullstack Engineer, unsatisfiably curious about
-          beautiful interfaces and relentlessly devoted to fine, detailed
-          polished functionalities.
-        </p>
-      </section>
-      <section className="flex items-center justify-center mt-10 sm:mt-30 mx-5 sm:mx-120 bg-white p-4 rounded-md">
-        <p className="text-[22px] sm:text-3xl text black">
-          Prince, Amanor. creating cool software that makes cool people&apos;s
-          life easier. Junior Fullstack Engineer, unsatisfiably curious about
-          beautiful interfaces and relentlessly devoted to fine, detailed
-          polished functionalities.
-        </p>
-      </section>
-      <section className="flex items-center justify-center mt-10 sm:mt-30 mx-5 sm:mx-120 bg-white p-4 rounded-md">
-        <p className="text-[22px] sm:text-3xl text black">
-          Prince, Amanor. creating cool software that makes cool people&apos;s
-          life easier. Junior Fullstack Engineer, unsatisfiably curious about
-          beautiful interfaces and relentlessly devoted to fine, detailed
-          polished functionalities.
-        </p>
-      </section>
-      <section className="flex items-center justify-center mt-10 sm:mt-30 mx-5 sm:mx-120 bg-white p-4 rounded-md">
-        <p className="text-[22px] sm:text-3xl text black">
-          Prince, Amanor. creating cool software that makes cool people&apos;s
-          life easier. Junior Fullstack Engineer, unsatisfiably curious about
-          beautiful interfaces and relentlessly devoted to fine, detailed
-          polished functionalities.
-        </p>
-      </section>
-      <section className="flex items-center justify-center mt-10 sm:mt-30 mx-5 sm:mx-120 bg-white p-4 rounded-md">
-        <p className="text-[22px] sm:text-3xl text black">
+      <section className="flex items-center justify-center mt-10 sm:mt-30 mx-5 sm:mx-120  p-4 rounded-md">
+        <p className="text-[22px] sm:text-3xl text-white">
           Prince, Amanor. creating cool software that makes cool people&apos;s
           life easier. Junior Fullstack Engineer, unsatisfiably curious about
           beautiful interfaces and relentlessly devoted to fine, detailed
